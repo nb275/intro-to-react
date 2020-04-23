5e897dfd5b462047b5eaca72fbf48e3eaad3433d
Nick Blenko <nb275@aim.com>
2020-04-23 16:48:22 -0600

    .
    .

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/intro-to-react/precache-manifest.b9908fe1aa0df6b4251959209c9f0799.js"
+  "/nb275/intro-to-react/precache-manifest.64fad252b814d7fc6c5041ef97d1b718.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/intro-to-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nb275/intro-to-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
